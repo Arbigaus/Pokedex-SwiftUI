4c218bdda9a7ab066b8cd4ab5e06bd268f2aae92
Gerson Arbigaus <gerson87@gmail.com>
2024-03-21 20:39:53 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,19 @@
+//
+//
+// PokedexApp.swift
+// Pokedex
+//
+// Created by Gerson Arbigaus on 21/03/24
+//
+        
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
