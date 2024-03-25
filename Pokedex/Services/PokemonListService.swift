220dcb6ec3360c2e6f290b9d9215aec005b7fd8e
Gerson Arbigaus <gerson87@gmail.com>
2024-03-24 21:06:25 -0300

    Create Service to fetch Pokemon list Making success test in the `PokemonListService` class

---
+++ Pokedex/Services/PokemonListService.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonListService.swift
+//  Pokedex
+//
+//  Created by Gerson Arbigaus on 24/03/24.
+//
+
+import Foundation
---
+++ PokedexTests/Mock/MockApiService.swift
@@ -0,0 +1,8 @@
+//
+//  MockApiService.swift
+//  PokedexTests
+//
+//  Created by Gerson Arbigaus on 24/03/24.
+//
+
+import Foundation
