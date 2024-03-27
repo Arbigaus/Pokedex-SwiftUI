31ffb15db3a5b104b6b477a6a1c91ba7049cd04c
Gerson Arbigaus <gerson87@gmail.com>
2024-03-27 16:40:03 -0300

    Create `PokemonListModel+Extensions`

--- PokedexTests/Extensions/PokemonListModel+Extensions.swift
+++ PokedexTests/Extensions/PokemonListModel+Extensions.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+@testable import Pokedex
+
+extension PokemonListModel {
+    static func mockPokeListModel() -> PokemonListModel {
+        let results = [
+            PokemonListItemModel(name: "Pikachu", url: "https://pokeapi.co/api/v2/pokemon/25"),
+            PokemonListItemModel(name: "Bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1"),
+            // Add more mock PokemonListItemModel instances as needed
+        ]
+        return PokemonListModel(count: results.count, next: "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20", previous: nil, results: results)
+    }
+}
+
+extension PokemonListModel: Equatable {
+    public static func == (lhs: PokemonListModel, rhs: PokemonListModel) -> Bool {
+        return lhs.results == rhs.results
+        && lhs.count == rhs.count
+        && lhs.next == rhs.next
+        && lhs.previous == rhs.previous
+    }
+}
